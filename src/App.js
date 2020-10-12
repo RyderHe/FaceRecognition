f97f20d59c43d1ebf992cf11e3b60974037c299a
RyderHe <r37he@edu.uwaterloo.ca>
2020-10-12 01:41:58 -0400

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,60 @@
 import React from 'react';
 import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 80,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    },
+    line_linked: {
+      shadow: {
+        enable: true,
+        color: "#ffffff",
+        blur: 5,
+        width: 1
+        // stroke: {
+        //   width: 5
+        // },
+      }
+    }
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: true,
+        mode: "repulse"
+      },
+      onclick: {
+        enable: true,
+        mode: "push"
+      }
+      // resize: true
+    }
+    
+  }
+}
 
 function App() {
   return (
     <div className="App">
-
+      <Particles className='particles'
+        params={particlesOptions}
+      />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+      {/* <FaceRecognition /> */}
     </div>
   );
 }
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () =>{
+    return (
+        <div>
+            <div className='white f1'>
+                {'Hey there, your current rank is ...'}
+            </div>
+            <div className='white f1'>
+                {'#10'}
+            </div>
+        </div>
+    );
+}
+
+export default Rank;
